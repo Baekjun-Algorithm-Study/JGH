fd8ed8903d4e86cea75da097af15a729bfe4e932
donasman <rjsgmlq33@gmail.com>
2025-01-13 21:45:04 +0900

    5주차 - 1: 2745 진수변환

---
+++ study/week05/P2745.java
@@ -0,0 +1,34 @@
+package study.week05;
+
+import java.util.Scanner;
+
+public class P2745 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+        
+        int result = 0;
+        
+        for(int i = 0; i < str.length(); i++) {
+        	char ch = str.charAt(str.length() - 1 - i);
+        	
+        	int value = 0;
+        	
+        	if(ch >= '0' && ch <= '9') {
+        		value = ch - '0';
+        	} else if(ch >= 'A' && ch <= 'Z') {
+        		value = ch - 'A' + 10;
+        	} else if(ch >= 'a' && ch <= 'z') {
+        		value = ch - 'a' + 10;
+        	}
+        	
+        	result += value * Math.pow(n, i);
+        	
+        	
+        }
+        System.out.println(result);
+        
+    }
+
+}
