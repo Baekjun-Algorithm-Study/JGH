559a7d36c9d043ddc39392d6001025e4081b2a9f
donasman <rjsgmlq33@gmail.com>
2025-01-14 21:08:07 +0900

    5주차 과제: 11005 진법 변환 2

--- study/week05/P11005.java
+++ study/week05/P11005.java
@@ -1,8 +1,9 @@
 package study.week05;
+
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class P11005 {
+	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int b = sc.nextInt();
@@ -25,4 +26,5 @@ public class Main {
         System.out.println(result.reverse().toString());
     
     }
+
 }
