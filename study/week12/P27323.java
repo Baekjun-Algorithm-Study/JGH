bd455500e4300ce6640a84d538def939f461683a
donasman <rjsgmlq33@gmail.com>
2025-04-01 22:51:21 +0900

    12주차 과제 완료

---
+++ study/week12/P27323.java
@@ -0,0 +1,17 @@
+package study.week12;
+
+import java.util.Scanner;
+
+public class P27323 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sc.close();
+        
+        System.out.println(a * b);
+    }
+
+}
